ae3b419d54fd5c5d9d21343f9e49faa4abf49629
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2025-12-20 15:41:51 +0700

    Add sample controller api
    Add sample controller api description

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+
+    [ApiController]
+    [Route(template: "sample-api")]
+    public class SampleController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public SampleController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(template: "users")]
+        public IEnumerable<UserModel> GetMockFullUser()
+        {
+            var faker = new Faker<UserModel>()
+                .RuleFor(u => u.Name, f => f.Name.FullName());
+
+            var users = faker.Generate(3);  // Tạo 100 user fake
+
+            return users;
+        }
+    }
+}
+
+public class UserModel
+{
+    public string Name { get; set; }
+}
