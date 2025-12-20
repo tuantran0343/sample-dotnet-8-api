df8434cf13372962bad04466b3f289b14245a564
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2025-12-20 16:01:47 +0700

    add new controller 01 (#2)

---
+++ Controllers/ContentV1Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route(template: "content-v1")]
+    public class ContentV1Controller : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public ContentV1Controller(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(template: "get-data")]
+        public IEnumerable<string> Get()
+        {
+            List<string> p3 = ["asd", "1222", "666"];
+            return p3;
+        }
+    }
+}
