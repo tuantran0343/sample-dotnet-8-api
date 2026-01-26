839232717b90defe55f9e92fdf3606f8e0345a6a
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2026-01-26 21:24:04 +0700

    chaneg text

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SampleAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
