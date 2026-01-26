23835c5a9bbb21d10718796ca5c5bf3ba6139317
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2026-01-26 21:28:50 +0700

    add weather string

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SampleAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Weather"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
